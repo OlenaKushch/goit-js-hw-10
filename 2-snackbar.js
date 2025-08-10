1410d1e29a689104b969c9c421f09ea802807c26
OlenaKushch <ellinate@gmail.com>
2025-08-10 12:57:20 +0000

    Deploying to gh-pages from @ OlenaKushch/goit-js-hw-10@fc824d72eee04792e4c6eed3deed7792897e8b54 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DOYGXuxJ.js";import{i}from"./assets/vendor-BbbuE1sJ.js";const s=document.querySelector(".form");s.addEventListener("submit",o=>{o.preventDefault();const t=Number(s.delay.value),r=s.state.value;new Promise((e,m)=>{setTimeout(()=>{r==="fulfilled"?e(t):m(t)},t)}).then(e=>{i.success({title:"✅ Success",message:`Fulfilled promise in ${e}ms`,position:"center"})}).catch(e=>{i.error({title:"❌ Error",message:`Rejected promise in ${e}ms`,position:"center"})})});
+//# sourceMappingURL=2-snackbar.js.map
